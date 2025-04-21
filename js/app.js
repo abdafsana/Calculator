4377b7570218fa41ced92546945df22b755cab03
abdafsana <abdullayevaynur527@gmail.com>
2025-04-21 12:27:23 +0400

    html and css

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const box = document.querySelectorAll('.conventor');
+box.forEach(item => {
+    item.addEventListener('click', () => {
+        box.forEach(b => b.classList.remove('active'));
+        item.classList.add('active');
+    });
+});
